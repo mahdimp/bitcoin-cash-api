d3709caaafff4e6adfe747113393dfbc7fcd0fb5
Mahdi MP <mahdi.mp@gmail.com>
2022-04-30 18:58:51 +0430

    Remove redundant comments

--- services/BitcoinCashService.ts
+++ services/BitcoinCashService.ts
@@ -1,7 +1,4 @@
-// REST API servers.
 const BCHN_MAINNET = 'https://bchn.fullstack.cash/v5/'
-
-// bch-js-examples require code from the main bch-js repo
 const BCHJS = require('@psf/bch-js')
 const bchjs = new BCHJS({ restURL: BCHN_MAINNET })
 
